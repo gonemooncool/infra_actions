d8739225e93197e53efc95f78f5d989f72bb8d2a
gonemooncool <anactus36@gmail.com>
2023-03-24 21:10:49 +0300

    site changes

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Я обязательно выживу!')
 
 
 def second_page(request):
